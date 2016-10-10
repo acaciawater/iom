6724fae310cb079995ac9c6c8e2bef23821285b8
theo <tkleinen@gmail.com>
2016-10-10 09:32:19 +0200

    exports all project locaties using tables from cartodb.config

--- iom/management/commands/export.py
+++ iom/management/commands/export.py
@@ -7,13 +7,15 @@ from django.core.management.base import BaseCommand
 from optparse import make_option
 from iom.models import Meetpunt, CartoDb 
 from iom.util import exportCartodb
+from acacia.data.models import ProjectLocatie
 
 class Command(BaseCommand):
     args = ''
     help = 'Exports all data to cartodb'
 
     def handle(self, *args, **options):
-        cdb = CartoDb.objects.get(pk=1)
-        exportCartodb(cdb, Meetpunt.objects.all(),'allemetingen')
+        for p in ProjectLocatie.objects.all():
+            mps = [m.meetpunt for m in p.meetlocatie_set.all()]
+            exportCartodb(p.cartodb, mps)
 
         
